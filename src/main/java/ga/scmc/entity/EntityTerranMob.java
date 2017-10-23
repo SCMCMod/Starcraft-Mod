78933e86af1af76bb2d2e2b10584fbbb1534f34f
Hypeirochus <Hypeirochus@gmail.com>
2017-10-23 16:41:46 -0400

    Tweaked terran mob spawning.

--- src/main/java/ga/scmc/entity/EntityTerranMob.java
+++ src/main/java/ga/scmc/entity/EntityTerranMob.java
@@ -1,11 +1,20 @@
 package ga.scmc.entity;
 
+import ga.scmc.handlers.ConfigurationHandler;
 import net.minecraft.world.World;
 
 public class EntityTerranMob extends EntityStarcraftMob {
 
 	public EntityTerranMob(World world) {
 		super(world);
+	}
+	
+	@Override
+	public boolean getCanSpawnHere() {
+		if(this.world.provider.getDimension() == ConfigurationHandler.INT_DIMENSION_KORHAL) {
+			return true;
+		}
+		return false;
 	}
 
 }
