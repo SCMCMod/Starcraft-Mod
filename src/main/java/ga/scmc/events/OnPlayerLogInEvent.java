73be93ca9865bf493fe3f799f301fe41462cf612
Hypeirochus <Hypeirochus@gmail.com>
2017-10-27 01:50:39 -0400

    Added a notification when players log in to give scmc version.

--- src/main/java/ga/scmc/events/OnPlayerLogInEvent.java
+++ src/main/java/ga/scmc/events/OnPlayerLogInEvent.java
@@ -3,7 +3,9 @@ package ga.scmc.events;
 import ga.scmc.debugging.ColorProvider;
 import ga.scmc.debugging.IColor;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.text.Style;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
 
@@ -12,11 +14,7 @@ public class OnPlayerLogInEvent {
 	@SubscribeEvent
 	public void onPlayerLogsIn(PlayerLoggedInEvent event) {
 		EntityPlayer player = event.player;
-
-		IColor color = player.getCapability(ColorProvider.COLOR, null);
-
-		String message = "Hello there, your team ID is " + color.getColor() + ".";
-
-		player.sendMessage(new TextComponentString(message));
+		String message = "Running SCMC version 1.6.";
+		player.sendMessage(new TextComponentString(message).setStyle(new Style().setColor(TextFormatting.RED)));
 	}
 }
