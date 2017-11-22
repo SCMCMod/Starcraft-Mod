232cfda513d1c4e4b4e6fbc19760e16efc330d6b
Hypeirochus <Hypeirochus@gmail.com>
2017-11-22 10:50:34 -0500

    Added kaldir.

--- src/main/java/ga/scmc/handlers/DimensionHandler.java
+++ src/main/java/ga/scmc/handlers/DimensionHandler.java
@@ -1,6 +1,7 @@
 package ga.scmc.handlers;
 
 import ga.scmc.worldgen.dimchar.WorldProviderChar;
+import ga.scmc.worldgen.dimkaldir.WorldProviderKaldir;
 import ga.scmc.worldgen.dimkorhal.WorldProviderKorhal;
 import ga.scmc.worldgen.dimshakuras.WorldProviderShakuras;
 import net.minecraft.world.DimensionType;
@@ -29,11 +30,12 @@ public class DimensionHandler {
 		shakuras_dt = DimensionType.register("Shakuras", "_starcraft", ConfigurationHandler.INT_DIMENSION_SHAKURAS, WorldProviderShakuras.class, true);
 		//slayn_dt = DimensionType.register("Slayn", "_starcraft", ConfigurationHandler.INT_DIMENSION_SLAYN, WorldProviderSlayn.class, true);
 		korhal_dt = DimensionType.register("Korhal", "_starcraft", ConfigurationHandler.INT_DIMENSION_KORHAL, WorldProviderKorhal.class, true);
-		//kaldir_dt = DimensionType.register("Kaldir", "_starcraft", ConfigurationHandler.INT_DIMENSION_SLAYN, WorldProviderKaldir.class, true);
+		kaldir_dt = DimensionType.register("Kaldir", "_starcraft", ConfigurationHandler.INT_DIMENSION_KALDIR, WorldProviderKaldir.class, true);
 		
 		DimensionManager.registerDimension(ConfigurationHandler.INT_DIMENSION_CHAR, char_dt);
 		DimensionManager.registerDimension(ConfigurationHandler.INT_DIMENSION_SHAKURAS, shakuras_dt);
 		DimensionManager.registerDimension(ConfigurationHandler.INT_DIMENSION_KORHAL, korhal_dt);
+		DimensionManager.registerDimension(ConfigurationHandler.INT_DIMENSION_KALDIR, kaldir_dt);
 		//DimensionManager.registerDimension(ConfigurationHandler.INT_DIMENSION_SLAYN, slayn_dt);
 	}
 }
