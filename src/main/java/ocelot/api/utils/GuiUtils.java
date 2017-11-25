27d1a8a97986d1eb235b1d3da2de627d57eb06eb
Ocelot5836 <FinntheRaider@gmail.com>
2017-11-24 16:02:46 -0800

    added another method to the gui utils class

--- src/main/java/ocelot/api/utils/GuiUtils.java
+++ src/main/java/ocelot/api/utils/GuiUtils.java
@@ -3,13 +3,18 @@ package ocelot.api.utils;
 import java.util.List;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.VertexBuffer;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 
 /**
  * <em><b>Copyright (c) 2017 Ocelot5836.</b></em>
  * 
  * @author Ocelot5836
  */
-public class GuiUtils {
+public class GuiUtils extends GuiScreen {
 
 	private static final Minecraft MC = Minecraft.getMinecraft();
 
@@ -53,5 +58,48 @@ public class GuiUtils {
 			return true;
 		}
 		return false;
+	}
+
+	/**
+	 * Draws a gui with the specified parameters.
+	 * 
+	 * @param x
+	 *            The x to draw the window at
+	 * @param y
+	 *            The y to draw the window at
+	 * @param width
+	 *            The width of the window
+	 * @param height
+	 *            The height of the window
+	 */
+	public static void drawCustomSizeGui(int x, int y, int width, int height, float zLevel) {
+		TextureUtils.bindTexture("textures/gui/util.png");
+		GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+		drawScaledCustomSizeModalRect(x + 5, y + 5, 5, 5, 5, 5, width - 10, height - 10, 256, 256);
+		drawScaledCustomSizeModalRect(x, y + 5, 0, 5, 5, 5, 5, height - 10, 256, 256);
+		drawScaledCustomSizeModalRect(x + width - 5, y + 5, 10, 5, 5, 5, 5, height - 10, 256, 256);
+		drawScaledCustomSizeModalRect(x + 5, y + height - 5, 5, 10, 5, 5, width - 10, 5, 256, 256);
+		drawScaledCustomSizeModalRect(x + 5, y, 5, 0, 5, 5, width - 10, 5, 256, 256);
+
+		drawStaticTextureModelRect(x, y, 0, 0, 5, 5, zLevel);
+		drawStaticTextureModelRect(x + width - 5, y, 10, 0, 5, 5, zLevel);
+		drawStaticTextureModelRect(x, y + height - 5, 0, 10, 5, 5, zLevel);
+		drawStaticTextureModelRect(x + width - 5, y + height - 5, 10, 10, 5, 5, zLevel);
+	}
+
+	/**
+	 * A static reference to {@link #drawTexturedModalRect(float, float, int, int, int, int)}.
+	 */
+	private static void drawStaticTextureModelRect(int x, int y, int textureX, int textureY, int width, int height, float zLevel) {
+		float f = 0.00390625F;
+		float f1 = 0.00390625F;
+		Tessellator tessellator = Tessellator.getInstance();
+		VertexBuffer vertexbuffer = tessellator.getBuffer();
+		vertexbuffer.begin(7, DefaultVertexFormats.POSITION_TEX);
+		vertexbuffer.pos((double) (x + 0), (double) (y + height), (double) zLevel).tex((double) ((float) (textureX + 0) * 0.00390625F), (double) ((float) (textureY + height) * 0.00390625F)).endVertex();
+		vertexbuffer.pos((double) (x + width), (double) (y + height), (double) zLevel).tex((double) ((float) (textureX + width) * 0.00390625F), (double) ((float) (textureY + height) * 0.00390625F)).endVertex();
+		vertexbuffer.pos((double) (x + width), (double) (y + 0), (double) zLevel).tex((double) ((float) (textureX + width) * 0.00390625F), (double) ((float) (textureY + 0) * 0.00390625F)).endVertex();
+		vertexbuffer.pos((double) (x + 0), (double) (y + 0), (double) zLevel).tex((double) ((float) (textureX + 0) * 0.00390625F), (double) ((float) (textureY + 0) * 0.00390625F)).endVertex();
+		tessellator.draw();
 	}
 }
