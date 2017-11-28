eaef0d59a921b4148fdcfcb41304f164518ccc7f
Ocelot5836 <FinntheRaider@gmail.com>
2017-11-28 15:11:36 -0800

    fixed a spelling error in the larva morph gui

--- src/main/java/ga/scmc/client/gui/GuiLists.java
+++ src/main/java/ga/scmc/client/gui/GuiLists.java
@@ -97,7 +97,7 @@ public class GuiLists {
 		larvaOptions.add(new LarvaOption(50, 0).setTooltip("Zergling"));
 		larvaOptions.add(new LarvaOption(75, 25).setTooltip("Roach"));
 		larvaOptions.add(new LarvaOption(100, 50).setTooltip("Hydralisk"));
-		larvaOptions.add(new LarvaOption(100, 150).setTooltip("Investor"));
+		larvaOptions.add(new LarvaOption(100, 150).setTooltip("Infestor"));
 		larvaOptions.add(new LarvaOption(100, 75).setTooltip("Swarm Host"));
 		larvaOptions.add(new LarvaOption(300, 200).setTooltip("Mutalisk"));
 		larvaOptions.add(new LarvaOption(0, 0).setTooltip("Test").addChild(new LarvaOption(100, 0).setTooltip("test2")).addChild(new LarvaOption(0, 100).setTooltip("test3")));
