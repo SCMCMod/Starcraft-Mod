c32eebd9dc4e04ce7e3682d5d6bbd952d43e2229
Hypeirochus <Hypeirochus@gmail.com>
2017-12-03 11:13:36 -0500

    Fixed kakaru's dropping protoss metal.

--- src/main/java/ga/scmc/entity/living/EntityKakaru.java
+++ src/main/java/ga/scmc/entity/living/EntityKakaru.java
@@ -82,12 +82,6 @@ public class EntityKakaru extends EntityFlying {
 	protected float getSoundVolume() {
 		return 5;
 	}
-		
-	@Override
-	protected void dropFewItems(boolean recentlyHit, int looting) {
-		ItemDrop drop = new ItemDrop(10, new ItemStack(ItemHandler.PROTOSS_INGOT, 1 + this.rand.nextInt(2), EnumMetaItem.ProtossIngotType.DARK.getID()));
-		drop.tryDrop(this);
-	}
 	
 	static class AILookAround extends EntityAIBase
     {
