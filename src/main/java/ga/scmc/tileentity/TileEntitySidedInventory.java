8a50d4b6718428dac37523943eb676e374e36abc
CJMinecraft01 <speed2pc2@gmail.com>
2017-12-04 19:36:07 +0000

    Update the sided inventory tile entity

--- src/main/java/ga/scmc/tileentity/TileEntitySidedInventory.java
+++ src/main/java/ga/scmc/tileentity/TileEntitySidedInventory.java
@@ -16,29 +16,43 @@ public class TileEntitySidedInventory extends TileEntity {
 
     protected ItemStackHandler handler;
     private SidedItemStackHandler northHandler, southHandler, eastHandler, westHandler, upHandler, downHandler;
+    private int[][] slotsForFace;
 
-    public TileEntitySidedInventory(int slotsDown, int slotsUp, int slotsNorth, int slotsSouth, int slotsWest, int slotsEast) {
-        this.handler = new ItemStackHandler(slotsDown + slotsUp + slotsNorth + slotsSouth + slotsWest + slotsEast) {
+
+    public TileEntitySidedInventory(int[] slotsDown, int[] slotsUp, int[] slotsNorth, int[] slotsSouth, int[] slotsWest, int[] slotsEast) {
+        this.slotsForFace = new int[][]{slotsDown, slotsUp, slotsNorth, slotsSouth, slotsWest, slotsEast};
+        this.handler = new ItemStackHandler(calculateSizeOfMainHandler()) {
             @Override
             public ItemStack insertItem(int slot, ItemStack stack, boolean simulate) {
                 if (!simulate)
-                    insertStack(slot, stack, simulate);
+                    insertStack(slot, stack);
                 return super.insertItem(slot, stack, simulate);
             }
 
             @Override
             public ItemStack extractItem(int slot, int amount, boolean simulate) {
                 if (!simulate)
-                    extractStack(slot, amount, simulate);
+                    extractStack(slot, amount);
                 return super.extractItem(slot, amount, simulate);
             }
         };
-        this.downHandler = new SidedItemStackHandler(slotsDown, EnumFacing.DOWN);
-        this.upHandler = new SidedItemStackHandler(slotsUp, EnumFacing.UP);
-        this.northHandler = new SidedItemStackHandler(slotsNorth, EnumFacing.NORTH);
-        this.southHandler = new SidedItemStackHandler(slotsSouth, EnumFacing.SOUTH);
-        this.westHandler = new SidedItemStackHandler(slotsWest, EnumFacing.WEST);
-        this.eastHandler = new SidedItemStackHandler(slotsEast, EnumFacing.EAST);
+        this.downHandler = new SidedItemStackHandler(slotsDown.length, EnumFacing.DOWN);
+        this.upHandler = new SidedItemStackHandler(slotsUp.length, EnumFacing.UP);
+        this.northHandler = new SidedItemStackHandler(slotsNorth.length, EnumFacing.NORTH);
+        this.southHandler = new SidedItemStackHandler(slotsSouth.length, EnumFacing.SOUTH);
+        this.westHandler = new SidedItemStackHandler(slotsWest.length, EnumFacing.WEST);
+        this.eastHandler = new SidedItemStackHandler(slotsEast.length, EnumFacing.EAST);
+    }
+
+    private int calculateSizeOfMainHandler() {
+        int lastSlot = 0;
+        for (int[] slots : this.slotsForFace) {
+            for (int slot : slots) {
+                if (slot > lastSlot)
+                    lastSlot = slot;
+            }
+        }
+        return lastSlot + 1;
     }
 
     private ItemStackHandler getHandlerForFace(EnumFacing side) {
@@ -59,106 +73,90 @@ public class TileEntitySidedInventory extends TileEntity {
         return this.handler;
     }
 
-    private int getMainSlotForFace(EnumFacing side, int slot) {
-        switch (side) {
-            case DOWN:
-                return slot;
-            case UP:
-                return slot + this.downHandler.getSlots();
-            case NORTH:
-                return slot + this.downHandler.getSlots() + this.upHandler.getSlots();
-            case SOUTH:
-                return slot + this.downHandler.getSlots() + this.upHandler.getSlots() + this.northHandler.getSlots();
-            case WEST:
-                return slot + this.downHandler.getSlots() + this.upHandler.getSlots() + this.northHandler.getSlots() + this.southHandler.getSlots();
-            case EAST:
-                return slot + this.downHandler.getSlots() + this.upHandler.getSlots() + this.northHandler.getSlots() + this.southHandler.getSlots() + this.westHandler.getSlots();
+    private void insertStack(int slot, ItemStack stack) {
+        for (int i = 0; i < this.slotsForFace.length; i++) {
+            for (int j = 0; j < this.slotsForFace[i].length; j++) {
+                if (this.slotsForFace[i][j] == slot) {
+                    switch (i) {
+                        case 0:
+                            this.downHandler.insertItem(j, stack, false);
+                            break;
+                        case 1:
+                            this.upHandler.insertItem(j, stack, false);
+                            break;
+                        case 2:
+                            this.northHandler.insertItem(j, stack, false);
+                            break;
+                        case 3:
+                            this.southHandler.insertItem(j, stack, false);
+                            break;
+                        case 4:
+                            this.westHandler.insertItem(j, stack, false);
+                            break;
+                        case 5:
+                            this.eastHandler.insertItem(j, stack, false);
+                            break;
+                    }
+                    break;
+                }
+            }
         }
-        return 0;
     }
 
-    private int getSlotForFace(EnumFacing side, int slot) {
-        switch (side) {
-            case DOWN:
-                return slot;
-            case UP:
-                return slot - this.downHandler.getSlots() + 1;
-            case NORTH:
-                return slot - this.downHandler.getSlots() - this.upHandler.getSlots() + 1;
-            case SOUTH:
-                return slot - this.downHandler.getSlots() - this.upHandler.getSlots() - this.northHandler.getSlots() + 1;
-            case WEST:
-                return slot - this.downHandler.getSlots() - this.upHandler.getSlots() - this.northHandler.getSlots() - this.southHandler.getSlots() + 1;
-            case EAST:
-                return slot - this.downHandler.getSlots() - this.upHandler.getSlots() - this.northHandler.getSlots() - this.southHandler.getSlots() - this.westHandler.getSlots() + 1;
+    private void extractStack(int slot, int amount) {
+        for (int i = 0; i < this.slotsForFace.length; i++) {
+            for (int j = 0; j < this.slotsForFace[i].length; j++) {
+                if (this.slotsForFace[i][j] == slot) {
+                    switch (i) {
+                        case 0:
+                            this.downHandler.extractItem(j, amount, false);
+                            break;
+                        case 1:
+                            this.upHandler.extractItem(j, amount, false);
+                            break;
+                        case 2:
+                            this.northHandler.extractItem(j, amount, false);
+                            break;
+                        case 3:
+                            this.southHandler.extractItem(j, amount, false);
+                            break;
+                        case 4:
+                            this.westHandler.extractItem(j, amount, false);
+                            break;
+                        case 5:
+                            this.eastHandler.extractItem(j, amount, false);
+                            break;
+                    }
+                    break;
+                }
+            }
         }
-        return 0;
-    }
-
-    private EnumFacing getFaceForSlot(int slot) {
-        if (slot < this.downHandler.getSlots())
-            return EnumFacing.DOWN;
-        if (slot < this.downHandler.getSlots() + this.upHandler.getSlots())
-            return EnumFacing.UP;
-        if (slot < this.downHandler.getSlots() + this.upHandler.getSlots() + this.northHandler.getSlots())
-            return EnumFacing.NORTH;
-        if (slot < this.downHandler.getSlots() + this.upHandler.getSlots() + this.northHandler.getSlots() + this.southHandler.getSlots())
-            return EnumFacing.SOUTH;
-        if (slot < this.downHandler.getSlots() + this.upHandler.getSlots() + this.northHandler.getSlots() + this.southHandler.getSlots() + this.westHandler.getSlots())
-            return EnumFacing.WEST;
-        return EnumFacing.EAST;
-    }
-
-    private ItemStack insertStack(int slot, ItemStack stack, boolean simulate) {
-        switch (getFaceForSlot(slot)) {
-            case DOWN:
-                return this.downHandler.insertItem(getSlotForFace(EnumFacing.DOWN, slot), stack, simulate);
-            case UP:
-                return this.upHandler.insertItem(getSlotForFace(EnumFacing.UP, slot), stack, simulate);
-            case NORTH:
-                return this.northHandler.insertItem(getSlotForFace(EnumFacing.NORTH, slot), stack, simulate);
-            case SOUTH:
-                return this.southHandler.insertItem(getSlotForFace(EnumFacing.SOUTH, slot), stack, simulate);
-            case WEST:
-                return this.westHandler.insertItem(getSlotForFace(EnumFacing.WEST, slot), stack, simulate);
-            case EAST:
-                return this.eastHandler.insertItem(getSlotForFace(EnumFacing.EAST, slot), stack, simulate);
-        }
-        return stack;
-    }
-
-    private ItemStack extractStack(int slot, int amount, boolean simulate) {
-        switch (getFaceForSlot(slot)) {
-            case DOWN:
-                return this.downHandler.extractItem(getSlotForFace(EnumFacing.DOWN, slot), amount, simulate);
-            case UP:
-                return this.upHandler.extractItem(getSlotForFace(EnumFacing.UP, slot), amount, simulate);
-            case NORTH:
-                return this.northHandler.extractItem(getSlotForFace(EnumFacing.NORTH, slot), amount, simulate);
-            case SOUTH:
-                return this.southHandler.extractItem(getSlotForFace(EnumFacing.SOUTH, slot), amount, simulate);
-            case WEST:
-                return this.westHandler.extractItem(getSlotForFace(EnumFacing.WEST, slot), amount, simulate);
-            case EAST:
-                return this.eastHandler.extractItem(getSlotForFace(EnumFacing.EAST, slot), amount, simulate);
-        }
-        return null;
     }
 
     private void updateHandlers() {
-        for (int slot = 0; slot < this.handler.getSlots(); slot++) {
-            if (slot < this.downHandler.getSlots())
-                this.downHandler.setStackInSlot(getSlotForFace(EnumFacing.DOWN, slot), this.handler.getStackInSlot(slot));
-            else if (slot < this.downHandler.getSlots() + this.upHandler.getSlots())
-                this.upHandler.setStackInSlot(getSlotForFace(EnumFacing.UP, slot), this.handler.getStackInSlot(slot));
-            else if (slot < this.downHandler.getSlots() + this.upHandler.getSlots() + this.northHandler.getSlots())
-                this.northHandler.setStackInSlot(getSlotForFace(EnumFacing.NORTH, slot), this.handler.getStackInSlot(slot));
-            else if (slot < this.downHandler.getSlots() + this.upHandler.getSlots() + this.northHandler.getSlots() + this.southHandler.getSlots())
-                this.southHandler.setStackInSlot(getSlotForFace(EnumFacing.SOUTH, slot), this.handler.getStackInSlot(slot));
-            else if (slot < this.downHandler.getSlots() + this.upHandler.getSlots() + this.northHandler.getSlots() + this.southHandler.getSlots() + this.westHandler.getSlots())
-                this.westHandler.setStackInSlot(getSlotForFace(EnumFacing.WEST, slot), this.handler.getStackInSlot(slot));
-            else
-                this.eastHandler.setStackInSlot(getSlotForFace(EnumFacing.EAST, slot), this.handler.getStackInSlot(slot));
+        for(int i = 0; i < this.slotsForFace.length; i++) {
+            for(int j = 0; j < this.slotsForFace[i].length; j++) {
+                switch (i) {
+                    case 0:
+                        this.downHandler.setStackInSlot(j, this.handler.getStackInSlot(this.slotsForFace[i][j]));
+                        break;
+                    case 1:
+                        this.upHandler.setStackInSlot(j, this.handler.getStackInSlot(this.slotsForFace[i][j]));
+                        break;
+                    case 2:
+                        this.northHandler.setStackInSlot(j, this.handler.getStackInSlot(this.slotsForFace[i][j]));
+                        break;
+                    case 3:
+                        this.southHandler.setStackInSlot(j, this.handler.getStackInSlot(this.slotsForFace[i][j]));
+                        break;
+                    case 4:
+                        this.westHandler.setStackInSlot(j, this.handler.getStackInSlot(this.slotsForFace[i][j]));
+                        break;
+                    case 5:
+                        this.eastHandler.setStackInSlot(j, this.handler.getStackInSlot(this.slotsForFace[i][j]));
+                        break;
+                }
+            }
         }
     }
 
@@ -201,14 +199,14 @@ public class TileEntitySidedInventory extends TileEntity {
         @Override
         public ItemStack insertItem(int slot, ItemStack stack, boolean simulate) {
             if (!simulate)
-                handler.insertItem(getMainSlotForFace(this.side, slot), stack, simulate);
+                handler.insertItem(slotsForFace[side.getIndex()][slot];, stack, simulate);
             return super.insertItem(slot, stack, simulate);
         }
 
         @Override
         public ItemStack extractItem(int slot, int amount, boolean simulate) {
             if (!simulate)
-                handler.extractItem(getMainSlotForFace(this.side, slot), amount, simulate);
+                handler.extractItem(slotsForFace[side.getIndex()][slot];, amount, simulate);
             return super.extractItem(slot, amount, simulate);
         }
     }
--- src/main/java/ga/scmc/tileentity/TileEntityStarcraftFurnace.java
+++ src/main/java/ga/scmc/tileentity/TileEntityStarcraftFurnace.java
@@ -1,9 +1,149 @@
 package ga.scmc.tileentity;
 
-public class TileEntityStarcraftFurnace extends TileEntitySidedInventory {
+/**
+ * A custom furnace which uses the {@link TileEntitySidedInventory} class
+ */
+public class TileEntityStarcraftFurnace extends TileEntitySidedInventory implements ITickable {
+
+    private int furnaceBurnTime;
+    private int currentItemBurnTime;
+    private int cookTime;
+    private int totalCookTime;
 
     public TileEntityStarcraftFurnace() {
-        super(2, 1, 1, 1, 1, 1);
+        super(new int[]{2, 1}, new int[]{0}, new int[]{1}, new int[]{1}, new int[]{1}, new int[]{1});
     }
 
+    public boolean isBurning() {
+        return this.furnaceBurnTime > 0;
+    }
+
+    private boolean canSmelt() {
+        if (this.handler.getStackInSlot(0) == null) {
+            return false;
+        } else {
+            ItemStack itemstack = FurnaceRecipes.instance().getSmeltingResult(this.handler.getStackInSlot(0));
+            if (itemstack == null)
+                return false;
+            if (this.handler.getStackInSlot(2) == null)
+                return true;
+            if (!this.handler.getStackInSlot(2).isItemEqual(itemstack))
+                return false;
+            int result = this.handler.getStackInSlot(2).stackSize + itemstack.stackSize;
+            return result <= this.handler.getSlots() && result <= this.handler.getStackInSlot(2).getMaxStackSize(); // Forge BugFix: Make it respect stack sizes properly.
+        }
+    }
+
+    public void smeltItem() {
+        if (this.canSmelt()) {
+            ItemStack itemstack = FurnaceRecipes.instance().getSmeltingResult(this.handler.getStackInSlot(0));
+
+            if (this.handler.getStackInSlot(2) == null) {
+                this.handler.setStackInSlot(2, itemstack.copy());
+            } else if (this.handler.getStackInSlot(2).getItem() == itemstack.getItem()) {
+                this.handler.getStackInSlot(2).stackSize += itemstack.stackSize; // Forge BugFix: Results may have multiple items
+            }
+
+            if (this.handler.getStackInSlot(0).getItem() == Item.getItemFromBlock(Blocks.SPONGE) && this.handler.getStackInSlot(0).getMetadata() == 1 && this.furnaceItemStacks[1] != null && this.furnaceItemStacks[1].getItem() == Items.BUCKET) {
+                this.handler.setStackInSlot(1, new ItemStack(Items.WATER_BUCKET));
+            }
+
+            --this.handler.getStackInSlot(0).stackSize;
+
+            if (this.handler.getStackInSlot(0).stackSize <= 0) {
+                this.handler.setStackInSlot(0, null);
+            }
+        }
+    }
+
+    public int getCookTime(@Nullable ItemStack stack) {
+        return 200;
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound nbt) {
+        super.readFromNBT(nbt);
+        this.furnaceBurnTime = nbt.getInteger("BurnTime");
+        this.cookTime = nbt.getInteger("CookTime");
+        this.totalCookTime = nbt.getInteger("CookTimeTotal");
+        this.currentItemBurnTime = TileEntityFurnace.getItemBurnTime(this.handler.getStackInSlot(1));
+    }
+
+    @Override
+    public NBTTagCompound writeToNBT(NBTTagCompound nbt) {
+        nbt.setInteger("BurnTime", this.furnaceBurnTime);
+        nbt.setInteger("CookTime", this.cookTime);
+        nbt.setInteger("CookTimeTotal", this.totalCookTime);
+        return super.writeToNBT(nbt);
+    }
+
+    @Override
+    public void update() {
+        boolean originalIsBurning = this.isBurning();
+        boolean burningState = false;
+
+        if (this.isBurning())
+        {
+            --this.furnaceBurnTime;
+        }
+
+        if (!this.world.isRemote)
+        {
+            if (this.isBurning() || this.handler.getStackInSlot(1) != null && this.handler.getStackInSlot(0) != null)
+            {
+                if (!this.isBurning() && this.canSmelt())
+                {
+                    this.furnaceBurnTime = TileEntityFurnace.getItemBurnTime(this.handler.getStackInSlot(1));
+                    this.currentItemBurnTime = this.furnaceBurnTime;
+
+                    if (this.isBurning())
+                    {
+                        burningState = true;
+
+                        if (this.handler.getStackInSlot(1) != null)
+                        {
+                            --this.handler.getStackInSlot(1).stackSize;
+
+                            if (this.handler.getStackInSlot(1).stackSize == 0)
+                            {
+                                this.handler.setStackInSlot(1, this.handler.getStackInSlot(1).getItem().getContainerItem(this.handler.getStackInSlot(1)));
+                            }
+                        }
+                    }
+                }
+
+                if (this.isBurning() && this.canSmelt())
+                {
+                    ++this.cookTime;
+
+                    if (this.cookTime == this.totalCookTime)
+                    {
+                        this.cookTime = 0;
+                        this.totalCookTime = this.getCookTime(this.handler.getStackInSlot(0));
+                        this.smeltItem();
+                        burningState = true;
+                    }
+                }
+                else
+                {
+                    this.cookTime = 0;
+                }
+            }
+            else if (!this.isBurning() && this.cookTime > 0)
+            {
+                this.cookTime = MathHelper.clamp(this.cookTime - 2, 0, this.totalCookTime);
+            }
+
+            if (originalIsBurning != this.isBurning())
+            {
+                burningState = true;
+                BlockFurnaceChar.setState(this.isBurning(), this.world, this.pos);
+            }
+        }
+
+        if (burningState)
+        {
+            this.markDirty();
+        }
+    }
 }
