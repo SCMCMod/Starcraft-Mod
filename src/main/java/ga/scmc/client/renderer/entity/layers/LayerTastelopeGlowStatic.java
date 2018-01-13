2e8018e15a349c6fa267be9dc29919bbc0041892
CJMinecraft01 <speed2pc2@gmail.com>
2018-01-13 13:23:27 +0000

    Removed duplicate code

--- src/main/java/ga/scmc/client/renderer/entity/layers/LayerTastelopeGlowStatic.java
+++ src/main/java/ga/scmc/client/renderer/entity/layers/LayerTastelopeGlowStatic.java
@@ -21,30 +21,7 @@ public class LayerTastelopeGlowStatic<T extends EntityTastelope> implements Laye
 
     @Override
     public void doRenderLayer(EntityTastelope entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
-        RENDERER.bindTexture(TEXTURE);
-        GlStateManager.enableBlend();
-        GlStateManager.enableAlpha();
-        GlStateManager.blendFunc(GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ONE);
-
-        if(entitylivingbaseIn.isInvisible()) {
-            GlStateManager.depthMask(false);
-        } else {
-            GlStateManager.depthMask(true);
-        }
-
-        int i = 61680;
-        int j = i % 65536;
-        int k = i / 65536;
-        OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, (float)j, (float)k);
-        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-        RENDERER.getMainModel().render(entitylivingbaseIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
-        i = entitylivingbaseIn.getBrightnessForRender(partialTicks);
-        j = i % 65536;
-        k = i / 65536;
-        OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, (float)j, (float)k);
-        RENDERER.setLightmap(entitylivingbaseIn, partialTicks);
-        GlStateManager.disableBlend();
-        GlStateManager.enableAlpha();
+        ColoredLayerRender.renderStaticGlow(this.RENDERER, entitylivingbaseIn, TEXTURE, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale, partialTicks);
     }
 
     @Override
