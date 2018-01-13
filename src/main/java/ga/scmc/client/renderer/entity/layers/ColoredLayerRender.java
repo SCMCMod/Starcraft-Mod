a5e7c4bdf3bb0a70436849e1cd136e407b9f08b7
CJMinecraft01 <speed2pc2@gmail.com>
2018-01-13 13:07:32 +0000

    Made coloured entities better

--- src/main/java/ga/scmc/client/renderer/entity/layers/ColoredLayerRender.java
+++ src/main/java/ga/scmc/client/renderer/entity/layers/ColoredLayerRender.java
@@ -1,24 +1,31 @@
 package ga.scmc.client.renderer.entity.layers;
 
+import ga.scmc.api.IEntityTeamColorable;
+import ga.scmc.entity.living.EntityStarcraftMob;
 import ga.scmc.entity.living.EntityStarcraftPassive;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderLiving;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.util.ResourceLocation;
 
 public class ColoredLayerRender {
 
-    public static <T extends EntityStarcraftPassive> void render(RenderLiving<T> renderer, EntityStarcraftPassive entity, ResourceLocation overlayTexture, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
-        renderer.bindTexture(overlayTexture);
-        GlStateManager.blendFunc(GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ONE);
+    public static <T extends EntityLiving> void render(RenderLiving<T> renderer, EntityLiving entity, ResourceLocation overlayTexture, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
+        if(entity instanceof IEntityTeamColorable) {
+            IEntityTeamColorable<EntityLiving> team = (IEntityTeamColorable<EntityLiving>) entity;
+            renderer.bindTexture(overlayTexture);
+            GlStateManager.blendFunc(GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ONE);
 
-        if(entity.isInvisible())
-            GlStateManager.depthMask(false);
-        else
-            GlStateManager.depthMask(true);
+            if(entity.isInvisible())
+                GlStateManager.depthMask(false);
+            else
+                GlStateManager.depthMask(true);
 
-        GlStateManager.color(entity.getTeamColor().getR() / 255.0F, entity.getTeamColor().getG() / 255.0F, entity.getTeamColor().getB() / 255.0F);
-        renderer.getMainModel().render(entity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
-        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+            GlStateManager.color(team.getTeamColor().getR() / 255.0F, team.getTeamColor().getG() / 255.0F, team.getTeamColor().getB() / 255.0F);
+            renderer.getMainModel().render(entity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
+            GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+        }
     }
 
 }
