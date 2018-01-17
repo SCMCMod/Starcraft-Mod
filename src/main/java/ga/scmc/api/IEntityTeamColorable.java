93c69fe0b7db23afa6b9ad6e3850acff84b7d45d
CJMinecraft01 <speed2pc2@gmail.com>
2018-01-17 16:50:20 +0000

    Commenting

--- src/main/java/ga/scmc/api/IEntityTeamColorable.java
+++ src/main/java/ga/scmc/api/IEntityTeamColorable.java
@@ -3,6 +3,11 @@ package ga.scmc.api;
 import ga.scmc.enums.EnumTeamColors;
 import net.minecraft.entity.EntityLiving;
 
+/**
+ * An interface used by the {@link ga.scmc.client.renderer.entity.layers.ColoredLayerRender}
+ * @author CJMinecraft
+ * @param <T> The entity class
+ */
 public interface IEntityTeamColorable<T extends EntityLiving> {
 
     EnumTeamColors getTeamColor();
