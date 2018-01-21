26161b2dc64577bc63a9bf04ff1c8821f4b35405
Hypeirochus <Hypeirochus@gmail.com>
2018-01-21 14:03:48 -0500

    Animated tastelope and artislope.

--- src/main/java/ga/scmc/client/renderer/model/ModelTastelope.java
+++ src/main/java/ga/scmc/client/renderer/model/ModelTastelope.java
@@ -721,6 +721,8 @@ public class ModelTastelope extends Model {
 
     @Override
     public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) { 
+		super.render(entity, f, f1, f2, f3, f4, f5);
+		setRotationAngles(f, f1, f2, f3, f4, f5, entity);
         this.chest.render(f5);
     }
     
