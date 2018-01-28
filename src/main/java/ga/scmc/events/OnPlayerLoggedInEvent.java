6fe866f11610c4e1a49e82d1fc9afc5f96bfa9af
Hypeirochus <Hypeirochus@gmail.com>
2018-01-28 11:20:34 -0500

    Version update.

--- src/main/java/ga/scmc/events/OnPlayerLoggedInEvent.java
+++ src/main/java/ga/scmc/events/OnPlayerLoggedInEvent.java
@@ -14,7 +14,7 @@ public class OnPlayerLoggedInEvent {
 	@SubscribeEvent
 	public void onPlayerLoggedInEvent(PlayerLoggedInEvent event) {
 		EntityPlayer player = event.player;
-		String message = "Running SCMC version 2.0.456!";
+		String message = "Running SCMC version 2.0.491!";
 		player.sendMessage(new TextComponentString(message).setStyle(new Style().setColor(TextFormatting.BLUE)));
 	}
 }
