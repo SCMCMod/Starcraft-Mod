d87a2291153c74366ff31c1a778e6a240c061ba1
Ocelot5836 <FinntheRaider@gmail.com>
2018-02-10 12:02:49 -0800

    added some new features to guis

---
+++ src/main/java/ga/scmc/client/gui/element/ComponentItemShopTab.java
@@ -0,0 +1,10 @@
+package ga.scmc.client.gui.element;
+
+import ocelot.api.client.gui.component.Component;
+
+public class ComponentItemShopTab extends Component {
+
+	public ComponentItemShopTab(int x, int y) {
+		super(x, y, 18, 18);
+	}
+}
