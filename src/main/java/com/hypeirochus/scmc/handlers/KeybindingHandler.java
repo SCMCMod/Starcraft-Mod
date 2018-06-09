632c876940cd07c21b21a74879b6975ec7fd4409
Hypeirochus <Hypeirochus@gmail.com>
2018-06-09 18:54:49 -0400

    Added two more ship ability keybindings.

--- src/main/java/com/hypeirochus/scmc/handlers/KeybindingHandler.java
+++ src/main/java/com/hypeirochus/scmc/handlers/KeybindingHandler.java
@@ -19,15 +19,21 @@ public class KeybindingHandler {
 
 	public static KeyBinding toggleMarineVisor;
 	public static KeyBinding shipAbilityOne;
+	public static KeyBinding shipAbilityTwo;
+	public static KeyBinding shipAbilityThree;
 
 	public static void pre(FMLPreInitializationEvent event) {
 		MinecraftForge.EVENT_BUS.register(new KeyEventHandler());
 
 		toggleMarineVisor = new KeyBinding("key.terran.marinevisor.toggle", Keyboard.KEY_V, STARCRAFT_CATEGORY);
-		shipAbilityOne = new KeyBinding("key.ship.ability.one", Keyboard.KEY_Z, STARCRAFT_CATEGORY);
+		shipAbilityOne = new KeyBinding("key.ship.ability.1", Keyboard.KEY_Z, STARCRAFT_CATEGORY);
+		shipAbilityTwo = new KeyBinding("key.ship.ability.2", Keyboard.KEY_X, STARCRAFT_CATEGORY);
+		shipAbilityThree = new KeyBinding("key.ship.ability.3", Keyboard.KEY_C, STARCRAFT_CATEGORY);
 
 		ClientRegistry.registerKeyBinding(toggleMarineVisor);
 		ClientRegistry.registerKeyBinding(shipAbilityOne);
+		ClientRegistry.registerKeyBinding(shipAbilityTwo);
+		ClientRegistry.registerKeyBinding(shipAbilityThree);
 	}
 
 	private static class KeyEventHandler {
@@ -40,6 +46,14 @@ public class KeybindingHandler {
 
 			if (KeybindingHandler.shipAbilityOne.isPressed()) {
 				NetworkHandler.sendToServer(new MessageShipAbility(0));
+			}
+
+			if (KeybindingHandler.shipAbilityTwo.isPressed()) {
+				NetworkHandler.sendToServer(new MessageShipAbility(1));
+			}
+
+			if (KeybindingHandler.shipAbilityThree.isPressed()) {
+				NetworkHandler.sendToServer(new MessageShipAbility(2));
 			}
 		}
 	}
